a794abb66da9de26de1692f5f2910f1db8793618
Thomas <t.girault@gmail.com>
2020-10-04 13:29:34 +0200

    Mise à jour de la rubrique mes clients.
    Mise à jour de la rubrique mes clients.

--- src/app/customers/services/customers.service.ts
+++ src/app/customers/services/customers.service.ts
@@ -14,12 +14,8 @@ export class CustomersService {
      */
     findAll(): Array<Company> {
         return Array.from([
-            new Company('Sopra Steria', 'logo-soprasteria', 'https://www.soprasteria.com/fr'),
             new Company('DSIA', 'logo-dsia', 'https://logistar-dsia.com/fr/'),
-            new Company('Douanes & Droits Indirects', 'logo-ddi', 'https://www.douane.gouv.fr/'),
-            new Company('Préfecture de Police', 'logo-prefecture-police', 'https://www.prefecturedepolice.interieur.gouv.fr/'),
-            new Company('Casier Judiciaire National', 'logo-cjn', 'https://casier-judiciaire.justice.gouv.fr/pages/accueil.xhtml'),
-            new Company('ENEDIS', 'logo-enedis', 'https://www.enedis.fr/')
+            new Company('Vous ?', 'logo-company', '')
         ]);
     }
 }
