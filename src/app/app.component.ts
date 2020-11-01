3049e60bc21776922c3b3c9a701eb6e091f3a41c
Thomas <t.girault@gmail.com>
2020-11-01 20:54:28 +0100

    Ajout du loader au chargement de la page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() { }
+  /** Indicateur de chargement. */
+  isLoaded = false;
+
+  /**
+   * Constructeur du composant.
+   * @param route une route
+   */
+  constructor(private route: Router) { }
+
+  /**
+   * Action à l'initialisation du composant.
+   */
+  ngOnInit(): void {
+    this.route.events.subscribe(
+      event => {
+        if (event instanceof NavigationStart) {
+          console.log('navigation starts');
+          this.isLoaded = true;
+
+        } else if (event instanceof NavigationEnd) {
+          console.log('navigation ends');
+          this.isLoaded = false;
+        }
+      },
+      error => {
+        this.isLoaded = false;
+        console.log(error);
+      });
+  }
 }
