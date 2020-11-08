81d4fc026d6a99478f2ccc3106fe89e037153a40
Thomas <t.girault@gmail.com>
2020-11-08 11:16:32 +0100

    Demande de consentement de la politique des cookies.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { NgcCookieConsentService, NgcInitializeEvent, NgcNoCookieLawEvent, NgcStatusChangeEvent } from 'ngx-cookieconsent';
+import { GoogleAnalyticsService } from 'ngx-google-analytics';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   /** Indicateur de chargement. */
   isLoaded = false;
 
+  /** Gestion du consentement des cookies. */
+  private popupOpenSubscription: Subscription;
+  private popupCloseSubscription: Subscription;
+  private initializeSubscription: Subscription;
+  private statusChangeSubscription: Subscription;
+  private revokeChoiceSubscription: Subscription;
+  private noCookieLawSubscription: Subscription;
+
   /**
    * Constructeur du composant.
    * @param route une route
+   * @param cookieConsentService service de gestion du consentement des cookies
+   * @param googleAnalyticsService service Google Analytics
    */
-  constructor(private route: Router) { }
+  constructor(
+    private route: Router,
+    private cookieConsentService: NgcCookieConsentService,
+    private googleAnalyticsService: GoogleAnalyticsService
+  ) { }
 
   /**
-   * Action à l'initialisation du composant.
+   * Initialisation du composant.
    */
   ngOnInit(): void {
     this.route.events.subscribe(
@@ -36,5 +53,84 @@ export class AppComponent implements OnInit {
         this.isLoaded = false;
         console.log(error);
       });
+
+    this.initCookieConsent();
+  }
+
+  /**
+   * Initialisation de la demande de consentement des cookies.
+   */
+  private initCookieConsent(): void {
+    // subscribe to cookieconsent observables to react to main events
+    this.popupOpenSubscription = this.cookieConsentService.popupOpen$.subscribe(
+      () => {
+        console.log('Ouverture de la popup de gestion du consentement.');
+      });
+
+    this.popupCloseSubscription = this.cookieConsentService.popupClose$.subscribe(
+      () => {
+        console.log('Fermeture de la popup de gestion du consentement.');
+      });
+
+    this.initializeSubscription = this.cookieConsentService.initialize$.subscribe(
+      (event: NgcInitializeEvent) => {
+        console.log('Initialisation de la popup de gestion du consentement.');
+      });
+
+    this.statusChangeSubscription = this.cookieConsentService.statusChange$.subscribe(
+      (event: NgcStatusChangeEvent) => {
+        console.log('Changement de statut de la popup de gestion du consentement.');
+
+        if (event.status === 'allow') {
+          this.allowCookies();
+        } else if (event.status === 'deny') {
+          this.denyCookies();
+        } else if (event.status === 'dismiss') {
+          this.denyCookies();
+        }
+      });
+
+    this.revokeChoiceSubscription = this.cookieConsentService.revokeChoice$.subscribe(
+      () => {
+        this.denyCookies();
+      });
+
+    this.noCookieLawSubscription = this.cookieConsentService.noCookieLaw$.subscribe(
+      (event: NgcNoCookieLawEvent) => {
+        console.log('No law de la popup de gestion du consentement.');
+      });
+  }
+
+  /**
+   * Autoriser les cookies.
+   */
+  allowCookies(): void {
+    this.googleAnalyticsService.gtag(
+      'create', {
+      'UA-179522815-1': 'auto'
+    });
+  }
+
+  /**
+   * Refuser les cookies.
+   */
+  denyCookies(): void {
+    console.log('Refuser les cookies.');
+    this.googleAnalyticsService.gtag('set', {
+      anonymizeIp: true,
+    });
+  }
+
+  /**
+   * Destruction du composant.
+   */
+  ngOnDestroy(): void {
+    // unsubscribe to cookieconsent observables to prevent memory leaks
+    this.popupOpenSubscription.unsubscribe();
+    this.popupCloseSubscription.unsubscribe();
+    this.initializeSubscription.unsubscribe();
+    this.statusChangeSubscription.unsubscribe();
+    this.revokeChoiceSubscription.unsubscribe();
+    this.noCookieLawSubscription.unsubscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgcCookieConsentConfig, NgcCookieConsentModule } from 'ngx-cookieconsent';
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+
+const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: 'epsitek.fr'
+  },
+  position: 'bottom-left',
+  theme: 'block',
+  palette: {
+    popup: {
+      background: '#17365d',
+      text: '#ffffff',
+      link: '#ffffff'
+    },
+    button: {
+      background: '#3e8914',
+      text: '#ffffff',
+      border: 'transparent'
+    }
+  },
+  type: 'opt-out',
+  content: {
+    message: 'Ce site web utilise des cookies pour analyser le trafic et vous assurer la meilleure expérience de navigation.',
+    dismiss: 'OK, j\'ai compris!',
+    deny: 'Refuser',
+    link: 'Plus d\'information',
+    href: 'https://www.epsitek.fr/mentions-legales',
+    policy: 'Politique des Cookies',
+    header: 'Cookies sur le site!',
+    allow: 'Autoriser les cookies'
+  }
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgcCookieConsentModule.forRoot(cookieConfig),
+    NgxGoogleAnalyticsModule.forRoot(environment.ga),
+    NgxGoogleAnalyticsRouterModule,
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
